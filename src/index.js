34d6e8d1ad736fd57b86b8d2cb2bbbd3bc3f985f
Bruno Henrique <brunohc2016@gmail.com>
2023-06-04 16:04:39 -0300

    global font sans-serif defined in aplication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { App } from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: "sans-serif",
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
